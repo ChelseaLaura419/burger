23c41d643dd25c316bc55a5f63c54feb7368af50
Chelsea Laura <cdesrosiers419@gmail.com>
2019-03-09 16:28:06 -0500

    Initial push, folder structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gourdon-Hamsey Burger (comes with squash & ham)', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Open Sesame Burger (served open-faced on a sesame seed bun)', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Blondes Have More Fun-gus Burger (comes with mushrooms)', true);
