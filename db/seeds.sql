5793258331298b2fa547c3d56849aa8b69577cd3
Chelsea Laura <cdesrosiers419@gmail.com>
2019-03-09 23:18:06 -0500

    Basic app up and working

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Gourdon-Hamsey Burger (comes with squash & ham)', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Open Sesame Burger (served open-faced on a sesame seed bun)', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Blondes Have More Fun-gus Burger (comes with mushrooms)', true);
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Black & Blue Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('BBQ Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Breakfast Burger', true);
+INSERT INTO burgers (name) VALUES ('Mushroom Swiss Burger');
